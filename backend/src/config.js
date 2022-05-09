da6cb729affcc8da33d729a4b0faaec2f6540f6e
Kornoto3434 <mphomakwala@outlook.com>
2022-05-10 00:52:41 +0200

    update contract

--- backend/src/config.js
+++ backend/src/config.js
@@ -63,9 +63,8 @@ const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a singl
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PUBLIC_MINT_START_DATE = "2022-05-01T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
-
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = null; // Optional. Eg:2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x0x9e1E558B89511a73b6378FFE324231456eC4EDC1"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
